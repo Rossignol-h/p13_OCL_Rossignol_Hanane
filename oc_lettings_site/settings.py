b868984943a0735a207e1e0a46b95f0fb2353887
Rossignol-h <ha.rossignol@laposte.net>
2023-01-17 21:14:09 +0100

    fix(settings.py): add whitenoise in middleware

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # add whitenoise
 ]
 
 ROOT_URLCONF = 'oc_lettings_site.urls'
